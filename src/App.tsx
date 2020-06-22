25f2a2547003068da64dab648e2f86d744115afc
Anderson <chltjdrhd777@naver.com>
2020-06-22 17:31:49 +0900

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Js1 from "./routes/Js1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/javascript" component={Js1} />
+    </Router>
   );
 }
 
---
+++ src/routes/Js1.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+//loop
+
+export default () => {
+  const anderson: any = {
+    name: "Anderson",
+    bills: [124, 48, 268, 180, 42],
+    calculation: function () {
+      this.tips = [];
+      this.finalValues = [];
+      for (let i = 0; i < this.bills.length; i++) {
+        let percentage = 0.1;
+        let checkList = this.bills[i];
+        if (checkList < 50) {
+          percentage = 0.2;
+        } else if (checkList >= 50 && checkList < 200) {
+          percentage = 0.15;
+        } else {
+          percentage = 0.1;
+        }
+        this.tips[i] = checkList * percentage;
+        this.finalValues[i] = checkList + checkList * percentage;
+      }
+    },
+  };
+  anderson.calculation();
+  console.log(anderson);
+
+  const mark: any = {
+    name: "Mark",
+    bills: [150, 25, 65, 85, 100],
+    calculation: function () {
+      this.finalValues = [];
+
+      const pushList = this.bills.map((every: number) => {
+        if (every < 50) {
+          return every * 0.2;
+        } else if (every >= 50 && every < 200) {
+          return every * 0.15;
+        } else {
+          return every * 0.1;
+        }
+      });
+      this.tips = pushList;
+    },
+  };
+  mark.calculation();
+  console.log(mark);
+
+  return <div>JsPractice</div>;
+};
